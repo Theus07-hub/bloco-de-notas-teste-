f8ae1371e7673b670584685375bfff130ae2b561
Matheus Campos <oliveiracampos797@gmail.com>
2026-07-12 21:38:34 -0300

    Enviando ajustes para auxilio de ia.

--- static/JS/botoes.js
+++ static/JS/botoes.js
@@ -69,3 +69,20 @@ saveButton.addEventListener("click", saveNote);
 deleteButton.addEventListener("click", deleteNote);
 prevButton.addEventListener("click", prevNote);
 nextButton.addEventListener("click", nextNote);
+
+
+// js pego no MDB cópiado//
+document.querySelector('.first-button').addEventListener('click', function () {
+
+document.querySelector('.animated-icon1').classList.toggle('open');
+});
+document.querySelector('.second-button').addEventListener('click', function () {
+
+document.querySelector('.animated-icon2').classList.toggle('open');
+});
+document.querySelector('.third-button').addEventListener('click', function () {
+
+document.querySelector('.animated-icon3').classList.toggle('open');
+});
+
+// Fim do código copiado.//
