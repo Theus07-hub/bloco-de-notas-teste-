2c5715b404af0ee6d73fd0cb41aa0f0479d51884
Campos.Mat <matheuscampos.mo@gmail.com>
2026-07-13 17:42:20 -0300

    Atualização de proporção de tamanho de caixa de texto

--- static/JS/botoes.js
+++ static/JS/botoes.js
@@ -71,18 +71,3 @@ prevButton.addEventListener("click", prevNote);
 nextButton.addEventListener("click", nextNote);
 
 
-// js pego no MDB cópiado//
-document.querySelector('.first-button').addEventListener('click', function () {
-
-document.querySelector('.animated-icon1').classList.toggle('open');
-});
-document.querySelector('.second-button').addEventListener('click', function () {
-
-document.querySelector('.animated-icon2').classList.toggle('open');
-});
-document.querySelector('.third-button').addEventListener('click', function () {
-
-document.querySelector('.animated-icon3').classList.toggle('open');
-});
-
-// Fim do código copiado.//
