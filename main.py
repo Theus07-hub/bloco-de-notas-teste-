f01adccd8d47f7049e923ebf177e42bd798030dd
Campos.Mat <matheuscampos.mo@gmail.com>
2026-07-23 16:59:45 -0300

    ATT de banco de dados para aba de bloco de notas.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from models import conexao_banco
+from views import *
 
 #Criando o App..
 app = Flask(__name__)#o conteudo "name" é para localizar aonde estamos em meio as pastas.
 
-from views import *
+@app.route("/")
+def home():
+    conexao= conexao_banco()
+    cursor= conexao.cursor()
+    cursor.execute("SELECT * FROM nota")
+    nota = cursor.fetchall() #método fetchall, ele vai listar as ultimas linhas como se fosse tuplas, e se não tiver nada.. ele returna uma lista vazia.
+    cursor.close()
+    conexao.close()
+    return str(nota)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
+#########################################
+
--- models.py
+++ models.py
@@ -1,1 +1,11 @@
 #Arquvo para salvar banco de dados.
+import mysql.connector
+
+def conexao_banco():
+    conexao=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="bloco_nota"
+    )
+    return conexao
